31af6b8825795ae21d5c6e08408bf1fd3d69ac59
CLx1024 <claire.tcnr@gmail.com>
2021-12-21 16:52:39 +0800

    211221 1652

---
+++ 0707_Imagecircle/src/main/java/tw/tcnr13/m0707/M0707.java
@@ -0,0 +1,48 @@
+package tw.tcnr13.m0707;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class M0707 extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView img01;
+    private ImageButton img02;
+    private TextView ans;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.m0707);
+        setupViewComponent();
+    }
+
+    private void setupViewComponent() {
+        img01 = (ImageView)findViewById(R.id.m0707_imgview);
+        img02 = (ImageButton)findViewById(R.id.m0707_imgbutton);
+        ans = (TextView)findViewById(R.id.m0707_t001);
+
+        img01.setOnClickListener(this);
+        img02.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {  // 監聽的第二種方法
+        switch(v.getId()){
+            case R.id.m0707_imgview:
+                ans.setText(getResources().getString(R.string.m0707_t001)+getString(R.string.m0707_t002));
+                break;
+            case R.id.m0707_imgbutton:
+                ans.setText(getResources().getString(R.string.m0707_t001)+getString(R.string.m0707_t003));
+                break;
+
+        }
+
+
+
+    }
+}
---
+++ 0709_Android_SHAPE/src/main/java/tw/tcnr13/m0709/RingActivity.java
@@ -0,0 +1,15 @@
+package tw.tcnr13.m0709;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ring);
+    }
+
+}
